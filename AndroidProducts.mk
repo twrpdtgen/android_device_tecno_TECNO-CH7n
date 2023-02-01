6786e2359fb702d3b0bf107c2b960380381a968d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-01 22:11:30 +0100

    TECNO-CH7n: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO CH7n Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CH7n.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CH7n-user \
+    omni_TECNO-CH7n-userdebug \
+    omni_TECNO-CH7n-eng
